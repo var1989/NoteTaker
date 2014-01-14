fa32437cbb086f17d4a6e9aa11fca6e30a74877a
Maheswaran M <maheswaranmoothiringode@gmail.com>
2014-01-14 19:31:12 +0530

    Display the message in the next activity

--- src/com/example/mysampleproject/DisplayMessageActivity.java
+++ src/com/example/mysampleproject/DisplayMessageActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.test.suitebuilder.annotation.Suppress;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 
 public class DisplayMessageActivity extends Activity {
@@ -17,8 +19,17 @@ public class DisplayMessageActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_message);
-		// Show the Up button in the action bar.
-		setupActionBar();
+		// Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(message);
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
 	}
 
 	/**
