13ba7431f48e99eb7bb3bf7c8f9aecb3b4f9b8cc
var1989 <maverick1989@gmail.com>
2014-01-15 22:58:07 +0530

    Saving the notes to a .txt file.

--- gen/com/example/mysampleproject/R.java
+++ gen/com/example/mysampleproject/R.java
@@ -24,8 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int edit_body=0x7f080001;
+        public static final int edit_header=0x7f080000;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -38,11 +39,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050004;
-        public static final int edit_message=0x7f050003;
+        public static final int button_add=0x7f050005;
+        public static final int edit_body=0x7f050004;
+        public static final int edit_header=0x7f050003;
         public static final int hello_world=0x7f050002;
-        public static final int title_activity_display_message=0x7f050006;
-        public static final int title_activity_main=0x7f050005;
+        public static final int msg_Display=0x7f050008;
+        public static final int title_activity_display_message=0x7f050007;
+        public static final int title_activity_main=0x7f050006;
     }
     public static final class style {
         /** 
--- src/com/example/mysampleproject/MainActivity.java
+++ src/com/example/mysampleproject/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.mysampleproject;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.sql.Savepoint;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 	
 	public static final String EXTRA_MESSAGE = "com.example.mysampleproject.MESSAGE";
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +37,55 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void sendMessage(View view)
+    public void addNote(View view)
     {
     	Log.i("myApp", "calling Send Message");
+    	Boolean status = false;
+    	//Intent intent =  new Intent(this,DisplayMessageActivity.class);
+    	EditText editText_header  = (EditText) findViewById(R.id.edit_header);
+    	EditText editText_body = (EditText) findViewById(R.id.edit_body);
+    	String message_header = editText_header.getText().toString();
+    	String message_body = editText_body.getText().toString();
+    	//intent.putExtra(EXTRA_MESSAGE,message);
+    	Log.i("myApp", "before calling SaveFile()");
+    	status =  SaveFile(message_header,message_body );
+    	if(status== true)
+    	{
+    		Log.i(EXTRA_MESSAGE, "File was created successfully");
+    		Toast.makeText(getApplicationContext(), "File was written successfully", Toast.LENGTH_SHORT).show();
+    	}
+    	else
+    	{
+    		Log.i(EXTRA_MESSAGE, "There was some error");
+    		Toast.makeText(getApplicationContext(), "There was some error. Check logs", Toast.LENGTH_SHORT).show();
+    	}
+    	//startActivity(intent);
     	
-    	Intent intent =  new Intent(this,DisplayMessageActivity.class);
-    	EditText editText  = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE,message);
-    	startActivity(intent);
-    	//>>>>>>> branch 'master' of https://github.com/var1989/NoteTaker.git
+    }
+    
+    public Boolean SaveFile(String subject, String body)
+    {
+    	Boolean status = false;
+    	Calendar timeStamp = Calendar.getInstance();
+    	String file_name = subject +""+ timeStamp.getTime().toString()+".txt";
+    	
+    	Log.i(EXTRA_MESSAGE,"Inside SaveFile() before opening the outputstream with filename -->"+file_name);
+    	StringBuilder sb = new StringBuilder();
+    	
+    	sb.append("HEADER :"+subject);
+    	sb.append("\nTIME :"+timeStamp);
+    	sb.append("\nBODY :"+body);	
+    	try
+    	{
+	    	OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(file_name,Context.MODE_PRIVATE));
+	    	outputStreamWriter.write(sb.toString());
+	    	outputStreamWriter.close();
+	    	status = true;
+    	}
+    	catch (IOException ioe) {
+    		Log.e(EXTRA_MESSAGE,"File Write Failed" +ioe.toString());
+		}
+    	
+    	return status;
     }
 }
